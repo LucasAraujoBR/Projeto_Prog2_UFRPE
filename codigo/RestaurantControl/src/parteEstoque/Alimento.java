8056fb67a95b6532e4d34715376210c6040e8486
Matheus Fidelis da Silva <mfds@a.recife.ifpe.edu.br>
2021-01-14 21:15:30 -0300

    Todos métodos do UML foram feitos, faltando implementação deles

--- codigo/RestaurantControl/src/parteEstoque/Alimento.java
+++ codigo/RestaurantControl/src/parteEstoque/Alimento.java
@@ -1,6 +1,24 @@
 package parteEstoque;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Alimento {
 
+	private String nomeProduto;
+	private String codigoBarra;
+	private LocalDate dataVencimento;
+	private double qtdProduto;
+	
+	DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public Alimento(String nomeProduto, String codigoBarra, String dataVencimento, double qtdProduto) {
+		this.nomeProduto = nomeProduto;
+		this.codigoBarra = codigoBarra;
+		this.dataVencimento = LocalDate.parse(dataVencimento, formatador);
+		this.qtdProduto = qtdProduto;
+	}
+	
+	
 	
 }
--- codigo/RestaurantControl/src/parteEstoque/Estoque.java
+++ codigo/RestaurantControl/src/parteEstoque/Estoque.java
@@ -1,6 +1,38 @@
 package parteEstoque;
 
-public class Estoque {
+import java.util.ArrayList;
 
+public class Estoque {
+	ArrayList<Alimento> alimentos;
 	
+	Estoque() {
+		
+		
+	}
+	
+	public void addEstoqueComida(String nomeProduto, String dataVencimento, String codigoBarra, double quantidade){
+		
+	}
+	
+	public void relatorioEstoque() {
+		
+	}
+	
+	public boolean decrementarEstoque() {
+		return true;
+	}
+	
+	public ArrayList<Alimento> pertoDeVencer(int numeroDiasParaVencer) {
+		ArrayList<Alimento> alimentosPertoDoVencimento = new ArrayList<Alimento>();
+		return alimentosPertoDoVencimento;
+	}
+	
+	public ArrayList<Alimento> estoqueEmBaixa() {
+		ArrayList<Alimento> alimentosEmBaixa = new ArrayList<Alimento>();
+		return alimentosEmBaixa;
+	}
+	
+	public void listarEstoque() {
+		
+	}
 }
