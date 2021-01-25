c186bf6d91a6acdd2a12136786d7def4de14e61e
Maike Menezes <maikermenezes@gmail.com>
2021-01-25 16:57:19 -0300

    Adicionando funções de fazer ou cancelar reservas

--- codigo/RestaurantControl/src/application/ReservaMesa.java
+++ codigo/RestaurantControl/src/application/ReservaMesa.java
@@ -9,20 +9,30 @@ public class ReservaMesa extends Mesa {
 	private LocalDateTime data;
 	private int numeroMesa;
 	private int qtdPessoas;
-	private ArrayList<String> reservas;
+	private ArrayList<ReservaMesa> reservas;
 	private String nomeReserva;
+	
+	public ReservaMesa() {
+		nomeReserva = "fulano";
+		numeroMesa = 0;
+	}
+	public ReservaMesa(String nome, int numero) {
+		nomeReserva = nome;
+		numeroMesa = numero;
+	}
 	
 	public boolean verificarDisponibilidade() {
 		return this.mesaOcupada;
 	}
 	
-	public void adicionarReserva() {
-		
+	public void adicionarReserva(ReservaMesa reserva) {
+		reservas.add(reserva);
 	}
 	
-	public void cancelarReserva() {
-		
+	public void cancelarReserva(ReservaMesa reserva) {
+		if (reservas.contains(reserva)) reservas.remove(reserva);
 	}
+	
 	
 	
 	
