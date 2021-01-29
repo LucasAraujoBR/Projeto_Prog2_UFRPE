d189cb1e7fb012b7a32e26ba247f986efd632b46
Lucas Araújo <lucaraj1412@gmail.com>
2021-01-29 01:54:01 -0300

    Adicionadas telas GUI iniciais

---
+++ TelasGUI/src/TelaCaixa/controllerCaixa.java
@@ -0,0 +1,31 @@
+package TelaCaixa;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class controllerCaixa {
+
+    @FXML
+    private Button BTMfecharConta;
+
+    @FXML
+    private Button BTMfecharCaixa;
+
+    @FXML
+    private ListView<?> listContasAbertas;
+    
+    @FXML
+	void acaoBTMFecharConta() {
+		/*IMPLEMENTAR AÇÃO DO BOTÃO FECHAR CONTA*/
+	}
+	@FXML
+	void acaoBTMFecharCaixa() {
+		/*IMPLEMENTAR AÇÃO DO BOTÃO FECHAR CAIXA*/
+	}
+	@FXML
+	void listarContas() {
+		/*IMPLEMENTAR LISTAGEM DE CONTAS*/
+	}
+
+}
---
+++ TelasGUI/src/TelaMesas/controllerMesas.java
@@ -0,0 +1,31 @@
+package TelaMesas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class controllerMesas {
+
+	@FXML
+	private Button BTMfecharMesas;
+
+	@FXML
+	private Button BTMabrirMesas;
+
+	@FXML
+	private ListView<?> listMesas;
+	
+	@FXML
+	void acaoBTMFechar() {
+		/*IMPLEMENTAR AÇÃO DO BOTÃO FECHAR MESA*/
+	}
+	@FXML
+	void acaoBTMAbrir() {
+		/*IMPLEMENTAR AÇÃO DO BOTÃO ABRIR MESA*/
+	}
+	@FXML
+	void listarMesas() {
+		/*IMPLEMENTAR LISTAGEM DE MESAS*/
+	}
+
+}
