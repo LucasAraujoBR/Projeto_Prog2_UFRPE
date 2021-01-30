e2beb4240b830132608a7f66e129e851d410a5a3
Lucas Araújo <lucaraj1412@gmail.com>
2021-01-30 01:27:12 -0300

    Coloquei os botões de ? e implementei listas de contas

--- TelasGUI/src/TelaCaixa/controllerCaixa.java
+++ TelasGUI/src/TelaCaixa/controllerCaixa.java
@@ -1,31 +1,68 @@
 package TelaCaixa;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 
-public class controllerCaixa {
+public class controllerCaixa implements Initializable{
 
     @FXML
     private Button BTMfecharConta;
 
     @FXML
     private Button BTMfecharCaixa;
-
+    /*SUBSTITUIR STRING PELA CLASSE CONTAS*/
     @FXML
-    private ListView<?> listContasAbertas;
+    private ListView<String> listContasAbertas;
+    @FXML
+    private Button BTMInter;
     
     @FXML
-	void acaoBTMFecharConta() {
-		/*IMPLEMENTAR AÇÃO DO BOTÃO FECHAR CONTA*/
+    void acaBTMInter(ActionEvent event) {
+    	Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+    	alerta.setTitle("Ajuda");
+    	alerta.setHeaderText(null);
+    	alerta.setContentText("Caixa\nFechar Conta: abrirá tela de pagamento e removerá a conta da lista\nFechar Caixa: finaliza o caixa e exibe tela com contas do dia\n");
+    	alerta.show();
+    }
+    @FXML
+	void listarContas(ActionEvent event) {
+		
+		listContasAbertas.getItems().addAll("Conta 01","Conta 02","Conta 03");
+		listContasAbertas.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	}
+    @FXML
+	void acaoBTMFecharConta(ActionEvent event) {
+		String sele = listContasAbertas.getSelectionModel().getSelectedItem();
+    	
+    	if(sele==null||sele.isEmpty()) {
+    		Alert alerta = new Alert(Alert.AlertType.WARNING);
+        	alerta.setTitle("ERRO");
+        	alerta.setHeaderText(null);
+        	alerta.setContentText("Favor selecionar uma Conta para executar");
+        	alerta.show();
+    	}
 	}
 	@FXML
 	void acaoBTMFecharCaixa() {
-		/*IMPLEMENTAR AÇÃO DO BOTÃO FECHAR CAIXA*/
+		Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+    	alerta.setTitle("Fechar caixa");
+    	alerta.setHeaderText(null);
+    	alerta.setContentText("Clique em OK para encerrar o caixa");
+    	alerta.show();
 	}
-	@FXML
-	void listarContas() {
-		/*IMPLEMENTAR LISTAGEM DE CONTAS*/
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		listarContas(null);
+		
 	}
 
 }
--- TelasGUI/src/TelaMesas/controllerMesas.java
+++ TelasGUI/src/TelaMesas/controllerMesas.java
@@ -1,6 +1,8 @@
 package TelaMesas;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
@@ -8,12 +10,24 @@ public class controllerMesas {
 
 	@FXML
 	private Button BTMfecharMesas;
-
+	@FXML
+    private Button BTMEditarMesas;
 	@FXML
 	private Button BTMabrirMesas;
-
+	@FXML
+    private Button BTMInter;
 	@FXML
 	private ListView<?> listMesas;
+	
+	@FXML
+	void acaoBTMInter(ActionEvent event) {
+		Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+    	alerta.setTitle("Ajuda");
+    	alerta.setHeaderText(null);
+    	alerta.setContentText("Mesas\nAbrir Mesas: iniciara uma mesa a lista\nEditar Mesas: Abrirá a tela de inserir pedidos e remover pedidos\n"
+    			+ "Fechar Mesas: Fechara a mesa selecionada");
+    	alerta.show();
+	}
 	
 	@FXML
 	void acaoBTMFechar() {
@@ -26,6 +40,11 @@ public class controllerMesas {
 	@FXML
 	void listarMesas() {
 		/*IMPLEMENTAR LISTAGEM DE MESAS*/
+	}
+	@FXML
+	void BTMEditarMesa() {
+		/*ABRIR TELA COM PEDIDOS E TOTAL PARCIAL DESSA MESA,
+		 * NESSA TELA TEM QUE TER A OPÇÃO DE ADICIONAR PEDIDOS OU REMOVER PEDIDOS*/
 	}
 
 }
