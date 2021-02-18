ccbff8075e8fbec92edeb476c4717a3c6f0d27b6
LuizMFL <luiz.m.franca@hotmail.com>
2021-02-17 22:13:27 -0300

    Cupom fiscal correto

--- TelasGUI/src/parteCaixa/Caixa.java
+++ TelasGUI/src/parteCaixa/Caixa.java
@@ -16,7 +16,7 @@ public class Caixa {
 	private double contaMesa;
 	private static ArrayList<Mesa> contasDoDia;
 	public Caixa() {
-		this.descricao = "";
+		this.descricao = "Caixa";
 		this.clienteDaMesa = null;
 		contaMesa = 0;
 		cofre = 0;
@@ -27,56 +27,25 @@ public class Caixa {
 	public void receberDinheiro() {
 		boolean recebeu = confirmarPagamento(gerarConta());
 		if(recebeu) {
-			gerarCupomFiscal(clienteDaMesa.getPedidos());
 			contasDoDia.add(clienteDaMesa);
 			clienteDaMesa = null;
 		}
-		else
-			System.out.println("***O valor a se receber é considerado uma subtração***");
 	}
-	// Precisa ainda receber da classe Pedido a DESCRIÇÃO do item pelo getDescrição()
-	// também seu preço por unidade e a quantidade por seus respectivos Gets
-	public void gerarCupomFiscal(ArrayList<pedCAD> pedidos) {
-		LocalDateTime data = LocalDateTime.now();
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-		int item = 1;
-		System.out.printf("%s   %s\n", data.toLocalDate().format(formato), data.toLocalTime().toString());
-		System.out.println("________________________________________________");
-		System.out.println("          C U P O M     F I S C A L");
-		System.out.println("ITEM          DESCRICAO");
-		System.out.println("QTD.   UN    VL.UNIT(R$)             VL.ITEM(R$)");
-		System.out.println("________________________________________________");
-		for(pedCAD atualPedido: pedidos) {
-			if(item >= 100)
-				System.out.printf ("%d         %s  \n", item,  atualPedido.getNome());
-			else if(item >= 10 )
-				System.out.printf ("0%d         %s  \n", item,  atualPedido.getNome());
-			else if(item >= 1) 
-				System.out.printf ("00%d         %s  \n", item,  atualPedido.getNome());
-			item += 1;
-			System.out.printf ("      %d.000 UN x %.2f                 %.2f\n", atualPedido.getQuantidade(), atualPedido.getValor(), (atualPedido.getValor() * atualPedido.getQuantidade()));
-		}
-		System.out.println("                                     -----------");
-		System.out.printf ("Subtotal R$                                %.2f\n", gerarConta());
-		System.out.println("________________________________________________");
-		System.out.printf("T O T A L  R $                              %.2f\n", gerarConta());
-		
-	} 
 	
 	public String cupomFiscal(ArrayList<pedCAD> pedidos) {
 		
 		LocalDateTime data = LocalDateTime.now();
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-		String nota = String.format("%s   %s\n", data.toLocalDate().format(formato), data.toLocalTime().toString());
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/YYYY   HH:mm:ss");
+		String nota = String.format("%s\n", data.format(formato));
 		String nota2 = String.format(("________________________________________________\n"));
 		String nota3 = String.format(("          C U P O M     F I S C A L\n"));
 		String nota4 = String.format(("ITEM          DESCRICAO\n"));
 		String nota5 = String.format(("QTD.   UN    VL.UNIT(R$)             VL.ITEM(R$)\n"));
 		String nota7 = String.format("________________________________________________\n");
-		String nota9 = String.format(("                                     -----------\n"));
-		String nota10 = String.format("Subtotal R$                                %.2f\n",controllerMesas.getMesaSelecionada().preco(pedidos));
+		String nota9 = String.format(("                                                    -----------\n"));
+		String nota10 = String.format("Subtotal R$                                      %.2f\n",controllerMesas.getMesaSelecionada().preco(pedidos));
 		String nota11 = String.format("________________________________________________\n");
-		String nota12 = String.format("T O T A L  R $                              %.2f\n", controllerMesas.getMesaSelecionada().preco(pedidos));
+		String nota12 = String.format("T O T A L  R $                                   %.2f\n", controllerMesas.getMesaSelecionada().preco(pedidos));
 		
         String nota8 = notaFiscal(pedidos,0);
         String cumpom = nota + nota2 + nota3 + nota4 + nota5+nota7+nota8;
@@ -100,9 +69,20 @@ public class Caixa {
 		else if(numero +1 >= 1) 
 		nota10 = String.format("00%d         %s  \n",numero+1,pedidos.get(numero).getNome());
 		
-		
-		String nota100 = String.format("      %d.000 UN x %.2f                 %.2f\n", pedidos.get(numero).getQuantidade(), pedidos.get(numero).getValor(), (pedidos.get(numero).getValor() * pedidos.get(numero).getQuantidade()));
-		
+		double conta = pedidos.get(numero).getValor() * pedidos.get(numero).getQuantidade();
+		String nota100 = String.format("      %d.000 UN x %.2f      ", pedidos.get(numero).getQuantidade(), pedidos.get(numero).getValor());
+		int tamanho = 51 - nota100.length();
+		if(conta >= 1000)
+			tamanho -= 7;
+		else if(conta >= 100)
+			tamanho -= 6;
+		else if(conta >= 10)
+			tamanho -= 5;
+		else
+			tamanho -= 4;
+		for(int comeco = 0; comeco < tamanho; comeco++)
+			nota100 += " ";
+		nota100 += String.format("%.2f\n", conta);
 		return nota10+ nota100;
 	}
 	
